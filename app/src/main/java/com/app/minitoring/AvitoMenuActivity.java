ff8d3f27eb407170ec96427fabd478a7cd367cae
Mikhaylo <prodomitor@gmail.com>
2021-04-10 22:17:28 +0200

    first commit m

---
+++ app/src/main/java/com/app/minitoring/AvitoMenuActivity.java
@@ -0,0 +1,13 @@
+package com.app.minitoring;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AvitoMenuActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_avito_menu);
+    }
+}
