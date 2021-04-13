d8c210281f0b4613d84890c43bde1e0f25243297
Mikhaylo <prodomitor@gmail.com>
2021-04-13 23:04:06 +0200

    can scrap data from a given url

--- app/src/main/java/com/app/minitoring/AvitoSearchActivity.java
+++ app/src/main/java/com/app/minitoring/AvitoSearchActivity.java
@@ -3,17 +3,25 @@ package com.app.minitoring;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.app.minitoring.category.AdModel;
 import com.app.minitoring.category.AppDatabase;
-import com.app.minitoring.category.CategoryModel;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class AvitoSearchActivity extends Activity {
     public static String EXTRA_TARGET_URL = "targetUrl";
-    List<CategoryModel> titleList = new ArrayList<>();
+    List<AdModel> titleList = new ArrayList<>();
     private AppDatabase db;
 
 
@@ -22,47 +30,49 @@ public class AvitoSearchActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_avito_search);
-//        LinearLayout containerLayout = findViewById(R.id.main_container);
+        LinearLayout containerLayout = findViewById(R.id.main_container);
 
         final TextView targetUrlView = findViewById(R.id.viewUrl);
         Intent intent = getIntent();
         String targetUlrText = intent.getStringExtra(EXTRA_TARGET_URL);
-        targetUrlView.setText("https://www.avito.ru/moskva/zapchasti_i_aksessuary/instrumenty-ASgBAgICAUQKxk0?cd=1&s=104");
+        targetUrlView.setText(targetUlrText);
 
-//        new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                try {
-//                    Document doc = Jsoup.connect("https://www.avito.ru/rossiya").timeout(60000).get();
-//                    Elements titles = doc.select(".footer-rubricator-block-230_v > ul > li > a");
-//                    for (Element title : titles) {
-//                        String url = String.format("%s%s", "https://www.avito.ru", title.attr("href"));
-//                        CategoryModel aCategory = new CategoryModel(title.text(), url);
-//                        titleList.add(aCategory);
-//                    }
-////                    titleText = title.text();
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//                runOnUiThread(new Runnable() {
-//                    @Override
-//                    public void run() {
-////                        Log.i("title", titleText);
-//                        for (CategoryModel categoryModel : titleList) {
-//                            Log.i("Category Name", categoryModel.getName());
-//                            Log.i("Category Url", categoryModel.getUrl());
-//                            Switch aSwitch = new Switch(AvitoSearchActivity.this);
-//                            TextView textView = new TextView(AvitoSearchActivity.this);
-//                            aSwitch.setText(categoryModel.getName());
-//                            textView.setText(categoryModel.getUrl());
-//                            containerLayout.addView(aSwitch);
-//                            containerLayout.addView(textView);
-//                        }
-////                        containerLayout.addView(linearLayout);
-//                    }
-//                });
-//            }
-//        }).start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Document doc = Jsoup.connect(targetUlrText).timeout(60000).get();
+                    Elements ads = doc.select(".iva-item-root-G3n7v");
+                    for (Element ad : ads) {
+                        String adName = ad.select("h3").text();
+                        String id = ad.attr("id");
+                        AdModel aCategory = new AdModel(adName, id);
+                        titleList.add(aCategory);
+                    }
+//                    titleText = title.text();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+//                        Log.i("title", titleText);
+                        for (AdModel categoryModel : titleList) {
+                            Log.i("title", categoryModel.getName());
+                            Log.i("id", categoryModel.getId());
+                            TextView nameView = new TextView(AvitoSearchActivity.this);
+                            TextView idView = new TextView(AvitoSearchActivity.this);
+                            nameView.setText(categoryModel.getName());
+                            idView.setText(categoryModel.getId());
+                            containerLayout.addView(nameView);
+//                            containerLayout.addView(idView);
+                        }
+//                        containerLayout.addView(linearLayout);
+
+                    }
+                });
+            }
+        }).start();
     }
 
 //    private void saveData() {
--- app/src/main/java/com/app/minitoring/category/AdModel.java
+++ app/src/main/java/com/app/minitoring/category/AdModel.java
@@ -1,12 +1,12 @@
 package com.app.minitoring.category;
 
-public class CategoryModel {
+public class AdModel {
     private String name;
-    private String url;
+    private String id;
 
-    public CategoryModel(String name, String url) {
+    public AdModel(String name, String id) {
         this.name = name;
-        this.url = url;
+        this.id = id;
     }
 
     public String getName() {
@@ -17,11 +17,11 @@ public class CategoryModel {
         this.name = name;
     }
 
-    public String getUrl() {
-        return url;
+    public String getId() {
+        return id;
     }
 
-    public void setUrl(String url) {
-        this.url = url;
+    public void setId(String id) {
+        this.id = id;
     }
 }
