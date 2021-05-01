e30a7d3b4ac1f2a2937266df02a93d67667928ab
Mikhaylo <prodomitor@gmail.com>
2021-05-01 21:29:46 +0200

    fixed a porblem with scanning in AdWorker

--- app/src/main/java/com/app/monitoring/AdWorker.java
+++ app/src/main/java/com/app/monitoring/AdWorker.java
@@ -7,7 +7,6 @@ import androidx.annotation.NonNull;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
-import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -34,41 +33,70 @@ public class AdWorker extends Worker {
         String targetUrl = getInputData().getString("targetUrl");
         System.out.println(targetUrl);
 
-        try {
-            // Connect
-            Connection con = Jsoup.connect(targetUrl)
-                    .userAgent("Chrome/90.0.4430.85")
-                    .referrer("http://www.google.com");
-//                    .timeout(10000)
-            // get response
-            Connection.Response res = con.execute();
-            Document doc = null;
 
-            if (res.statusCode() == 200) {
-                doc = con.get();
-                Elements ads = doc.select(".iva-item-root-G3n7v");
-//            System.out.println("IN WORKER");
-            for (Element ad : ads) {
-                System.out.println("IN WORKER");
-                Element heading = ad.selectFirst(".iva-item-titleStep-2bjuh");
-                String headingHref = heading.selectFirst("a").attr("href");
 
-                String adName = ad.select("h3").text();
-                String id = ad.attr("id");
-                headingHref = avitoBaseUrl.concat(headingHref);
-                System.out.println(String.format("%s: %s - %s!!!", CLASS_TAG, adName, id));
-                Ad adRecord = new Ad(id, adName, headingHref, targetUrl);
-                repo.insertAd(adRecord);
-//                System.out.println();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Document doc = Jsoup.connect(targetUrl).get();
+                    Elements ads = doc.select(".iva-item-root-G3n7v");
+                    for (Element ad : ads) {
+                        Element heading = ad.selectFirst(".iva-item-titleStep-2bjuh");
+                        String headingHref = heading.selectFirst("a").attr("href");
+                        String adName = ad.select("h3").text();
+                        // avito ad id
+                        String id = ad.attr("id");
+                        headingHref = avitoBaseUrl.concat(headingHref);
+                        Ad scrapedAd = new Ad(id, adName, headingHref, targetUrl);
+                        Log.i("NEW ADD", String.format("%s: %s - %s", TAG, adName, id));
+                        repo.insertAd(scrapedAd);
+                        }
+                    } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                }
             }
-            return Result.success();
-            } else {
-                Log.i(TAG, "Connection failed");
-                return Result.failure();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-            return Result.failure();
-        }
+        }).start();
+
+        return Result.success();
+
+
+
+
+//        try {
+//            // Connect
+//            Connection con = Jsoup.connect(targetUrl)
+//                    .userAgent("Chrome/90.0.4430.85")
+//                    .referrer("http://www.google.com");
+////                    .timeout(10000)
+//            // get response
+//            Connection.Response res = con.execute();
+//            Document doc = null;
+//
+//            if (res.statusCode() == 200) {
+//                doc = con.get();
+//                Elements ads = doc.select(".iva-item-root-G3n7v");
+////            System.out.println("IN WORKER");
+//            for (Element ad : ads) {
+//                System.out.println("IN WORKER");
+//                Element heading = ad.selectFirst(".iva-item-titleStep-2bjuh");
+//                String headingHref = heading.selectFirst("a").attr("href");
+//                String adName = ad.select("h3").text();
+//                String id = ad.attr("id");
+//                headingHref = avitoBaseUrl.concat(headingHref);
+//                System.out.println(String.format("%s: %s - %s!!!", CLASS_TAG, adName, id));
+//                Ad adRecord = new Ad(id, adName, headingHref, targetUrl);
+//                repo.insertAd(adRecord);
+////                System.out.println();
+//            }
+//            return Result.success();
+//            } else {
+//                Log.i(TAG, "Connection failed");
+//                return Result.failure();
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//            return Result.failure();
+//        }
     }
 }
