2a9777a3a5c0e466b3e34126cd224e29a1563394
Mikhaylo <prodomitor@gmail.com>
2021-05-01 21:38:33 +0200

    fixed

--- app/src/main/java/com/app/monitoring/AdWorker.java
+++ app/src/main/java/com/app/monitoring/AdWorker.java
@@ -32,9 +32,6 @@ public class AdWorker extends Worker {
     public Result doWork() {
         String targetUrl = getInputData().getString("targetUrl");
         System.out.println(targetUrl);
-
-
-
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -59,44 +56,5 @@ public class AdWorker extends Worker {
         }).start();
 
         return Result.success();
-
-
-
-
-//        try {
-//            // Connect
-//            Connection con = Jsoup.connect(targetUrl)
-//                    .userAgent("Chrome/90.0.4430.85")
-//                    .referrer("http://www.google.com");
-////                    .timeout(10000)
-//            // get response
-//            Connection.Response res = con.execute();
-//            Document doc = null;
-//
-//            if (res.statusCode() == 200) {
-//                doc = con.get();
-//                Elements ads = doc.select(".iva-item-root-G3n7v");
-////            System.out.println("IN WORKER");
-//            for (Element ad : ads) {
-//                System.out.println("IN WORKER");
-//                Element heading = ad.selectFirst(".iva-item-titleStep-2bjuh");
-//                String headingHref = heading.selectFirst("a").attr("href");
-//                String adName = ad.select("h3").text();
-//                String id = ad.attr("id");
-//                headingHref = avitoBaseUrl.concat(headingHref);
-//                System.out.println(String.format("%s: %s - %s!!!", CLASS_TAG, adName, id));
-//                Ad adRecord = new Ad(id, adName, headingHref, targetUrl);
-//                repo.insertAd(adRecord);
-////                System.out.println();
-//            }
-//            return Result.success();
-//            } else {
-//                Log.i(TAG, "Connection failed");
-//                return Result.failure();
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//            return Result.failure();
-//        }
     }
 }
