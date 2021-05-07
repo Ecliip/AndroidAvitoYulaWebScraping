f281147d3c01ebd9290bdd524d7a6f4460e3979a
Mikhaylo <prodomitor@gmail.com>
2021-05-07 12:11:41 +0200

    started working on Scanning Service

---
+++ app/src/main/java/com/app/monitoring/ScanningService.java
@@ -0,0 +1,19 @@
+package com.app.monitoring;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+// TODO implement this service later
+public class ScanningService extends IntentService {
+
+    public ScanningService() {
+        super("ScanningService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+    }
+
+
+}
