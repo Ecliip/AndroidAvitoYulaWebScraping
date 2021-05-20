b74fa8b4feef530b60ffa7f8e8be2d678f68da1c
Mikhaylo <prodomitor@gmail.com>
2021-05-20 21:20:37 +0200

    created embedded activity

---
+++ app/src/main/java/com/app/monitoring/EmbeddedPageActivity.java
@@ -0,0 +1,14 @@
+package com.app.monitoring;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EmbeddedPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_embedded_page);
+    }
+}
