053d18e629b463b4774bcac85c3f2351b1bb299d
Mikhaylo <prodomitor@gmail.com>
2021-05-21 13:02:21 +0200

    ready adding custom tab dependency

--- app/src/main/java/com/app/monitoring/EmbeddedPageActivity.java
+++ app/src/main/java/com/app/monitoring/EmbeddedPageActivity.java
@@ -1,14 +1,22 @@
 package com.app.monitoring;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import androidx.appcompat.app.AppCompatActivity;
+import com.app.monitoring.databinding.ActivityEmbeddedPageBinding;
 
 public class EmbeddedPageActivity extends AppCompatActivity {
+    private ActivityEmbeddedPageBinding binding;
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_embedded_page);
+        binding = ActivityEmbeddedPageBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        webView = binding.myWebView;
+        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
     }
 }
